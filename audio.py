78755d8c86cb7f0a32d91830a3672a797f648540
dhfx3712 <dhfx_wy@msn.com>
2020-11-11 20:06:33 +0800

    增加audio方法

--- audio.py
+++ audio.py
@@ -209,6 +209,12 @@ def generate_feature():
 
 
 
+
+
+
+
+
+
             
 
 class voice1():
@@ -277,6 +283,166 @@ class voice1():
         print (len(self.high_point),self.high_point)
 
 
+
+
+'''
+#采样点大于频率的2倍。blocks_size=采样率/frames的结果进行分割。每秒被分割为frames个窗口
+#fft按blocks_size大小进行变换。（注意2倍频率可能超出blocks_size范围）
+#是否考虑平移和滚动窗口？
+#第几秒作为关键数据。或者几秒钟的窗口数据作为关键数据
+#fft函数给了一个时间窗口min_c
+
+'''
+class voice2():
+    def loaddata(self, filepath):
+        '''
+        :param filepath: 文件路径，为wav文件
+        :return: 如果无异常则返回True，如果有异常退出并返回False
+        self.wave_data内储存着多通道的音频数据，其中self.wave_data[0]代表第一通道
+        具体有几通道，看self.nchannels
+        '''
+        if type(filepath) != str:
+            raise (TypeError, 'the type of filepath must be string')
+        p1 = re.compile('\.wav')
+        if p1.findall(filepath) is None:
+            raise (IOError, 'the suffix of file must be .wav')
+        try:
+            f = wave.open(filepath, 'rb')
+            params = f.getparams()
+            #print (params,type(params))
+            self.nchannels, self.sampwidth, self.framerate, self.nframes = params[:4]
+            #print (self.nchannels,self.sampwidth, self.framerate,self.nframes)
+            str_data = f.readframes(self.nframes)
+            #测试nframes
+            #str_data = f.readframes(16)
+            self.wave_data = np.fromstring(str_data, dtype=np.short)
+            #print (self.wave_data,self.wave_data.shape)
+            self.wave_data.shape = -1, self.sampwidth
+            #self.wave_data.shape = -1 此处是一个整体（-1.2）的形式
+            #print (self.wave_data)
+            #print (self.wave_data.shape)
+            
+            self.wave_data = self.wave_data.T
+            #print (self.wave_data)
+            f.close()
+            self.name = os.path.basename(filepath)  # 记录下文件名
+            return True
+        except:
+            raise (IOError, 'File Error')
+ 
+    def fft(self, frames=20,min_c=1):
+        '''
+        整体指纹提取的核心方法，将整个音频分块后分别对每块进行傅里叶变换，之后分子带抽取高能量点的下标
+        :param frames: frames是指定每秒钟分块数
+        :return:
+        '''
+        block = []
+        fft_blocks = []
+        self.high_point = []
+        blocks_size = self.framerate // frames  # block_size为每一块的frame数量
+        #print (blocks_size)
+        blocks_num = self.nframes // blocks_size  # 将音频分块的数量
+
+        #self.play_figure(blocks_size)
+
+
+        
+        #print (blocks_num)
+        #for i in range(0, len(self.wave_data[0]) - blocks_size, blocks_size):
+
+        min_num = frames*min_c
+        for i in range(0, min_num*blocks_size, blocks_size): 
+            block.append(self.wave_data[0][i:i + blocks_size])
+            #print (np.fft.fft(self.wave_data[0][i:i + blocks_size]))
+            fft_blocks.append(np.abs(np.fft.fft(self.wave_data[0][i:i + blocks_size])))
+            
+            #print (fft_blocks,len(fft_blocks[0]),len(fft_blocks))
+            '''
+            self.high_point.append((np.argmax(fft_blocks[-1][:40]),
+                                    np.argmax(fft_blocks[-1][40:80]) + 40,
+                                    np.argmax(fft_blocks[-1][80:120]) + 80,
+                                    np.argmax(fft_blocks[-1][120:180]) + 120,
+                                    np.argmax(fft_blocks[-1][180:300]) + 180,
+                                    np.argmax(fft_blocks[-1][300:600]) + 300,
+                                    ))
+            '''
+        import functools
+        import operator
+        import itertools
+        #print (len(fft_blocks),fft_blocks)
+        #fft_blocks = functools.reduce(operator.concat,fft_blocks)
+        #平铺方法https://www.cnblogs.com/wushaogui/p/9241931.html
+        fft_blocks = list(itertools.chain.from_iterable(fft_blocks))
+        #print (len(fft_blocks),fft_blocks[-10:])
+
+        #print ([np.argmax(fft_blocks[1:i+10]) for i in range(3)])
+
+        #print ([np.argmax(fft_blocks[i*blocks_size:(i+1)*blocks_size])+ i*blocks_size for i in range(40)])
+        self.high_point = [np.argmax(fft_blocks[i*blocks_size:(i+1)*blocks_size])+ i*blocks_size for i in range(min_num)]
+        #print (len(self.high_point),self.high_point)
+        return self.high_point
+
+        
+
+
+
+    def play_figure(self,blocks_size):  
+        y = np.fft.fft(self.wave_data[0][30*blocks_size:31*blocks_size])
+        x = np.arange(blocks_size)
+        abs_y = np.abs(y)
+        normalization_y=abs_y/ blocks_size
+
+        plt.subplot(121)
+        plt.plot(x,abs_y,'black')
+
+        plt.subplot(122)
+        plt.plot(x,normalization_y,'black')
+
+
+        #设置关闭时间
+        plt.pause(3)
+        plt.close()
+        #plt.show()
+
+
+def generate_feature1():
+    p = voice2()
+
+    audioList = os.listdir('../sample')
+    #print (audioList)
+
+
+    for k in range(1,3):
+        feature = {}
+        data = []
+        for tmp in audioList:
+            audioName = os.path.join('../sample', tmp)
+            #print (audioName)
+            if audioName.endswith('.wav'):
+                p.loaddata(audioName)
+                temp = p.fft(min_c=k)
+                #print (temp[0],type(temp[0]))
+                feature[tmp] = temp
+                #print (feature)
+        keys = list(feature.keys())
+        #print (keys)
+        #print (feature)
+        
+        for i in range(len(keys)-1):
+            for j in range(i+1,len(keys)):
+                #print (keys[i],keys[j],np.dot(feature[keys[i]],feature[keys[j]]))
+                #print (cosine_similarity([feature[keys[i]],feature[keys[j]]]))
+                
+                data.append([keys[i],keys[j],feature[keys[i]],feature[keys[j]],cosine_similarity([feature[keys[i]],feature[keys[j]]])[0][1]])
+        pd_data = pd.DataFrame(data,columns=["itemA","itemB","itemA_feature","itemB_feature","sim"])
+        pd_data.to_csv('./sim_%d.csv'%k,index=False)
+        
+        
+
+
+
+
+
 def fft_test():
     a = np.array([1,2,3])
     pdb.set_trace()
@@ -285,6 +451,22 @@ def fft_test():
 
 
 
+
+
+def test_1():
+    p = voice1()
+    #bofang
+    #p.play('./zdqc.wav')
+    p.loaddata('../sample/2A.wav')
+    p.fft()
+
+
+def test_2():
+    p = voice2()
+    #bofang
+    #p.play('./zdqc.wav')
+    p.loaddata('../sample/2A.wav')
+    p.fft(min_c=2)
 
 
 
@@ -305,13 +487,11 @@ if __name__ == '__main__':
     '''    
 
 
-    p = voice1()
-    #bofang
-    #p.play('./zdqc.wav')
-    p.loaddata('./sample/2A.wav')
-    p.fft()
+
     
     #fft_test()
+    #test_2()
+    generate_feature1()
 
 
 
